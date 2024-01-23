bcbb3691db7b73caacde3df17dd9d12f91a6cf48
Jose RM <mojica.raul@gmail.com>
2024-01-23 10:15:16 -0600

    Create the scaffold of food

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_22_210722) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_025731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
