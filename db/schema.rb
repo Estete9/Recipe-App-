f51104eaef9580b9bb3acd3c56d2fcc33acad353
Erwin Amador <eamador0305@gmail.com>
2024-01-23 11:57:37 -0600

    create recipes model and migrate table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_22_210722) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_175201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "preparation_time"
+    t.datetime "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_recipes_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,10 +32,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_22_210722) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users", column: "author_id"
 end
