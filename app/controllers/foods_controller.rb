df4b1d76866a40ff7b95beb96daed5cdf4c64b38
Jose RM <mojica.raul@gmail.com>
2024-01-23 16:00:28 -0600

    Create new migration to add reference in the foods table and delete some messages

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -19,7 +19,7 @@ class FoodsController < ApplicationController
 
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    @food = Food.new(food_params.merge(user: current_user))
 
     respond_to do |format|
       if @food.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_23_025731) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_215329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,11 +20,35 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_025731) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "preparation_time"
+    t.datetime "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_recipes_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -34,4 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_025731) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "inventories", "users"
+  add_foreign_key "recipes", "users", column: "author_id"
 end
