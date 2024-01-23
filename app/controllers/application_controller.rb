dec5c3858bc5407e557a496bb5ab497215779e25
Esteban <cepalacios@outlook.com>
2024-01-23 09:21:25 -0500

    Update sign up view and application controller to take :name parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_account_params, if: :devise_controller?
+
+  private
+
+  def configure_account_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+  end
 end
--- db/migrate/20240122210722_devise_create_users.rb
+++ db/migrate/20240122210722_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
 
       t.timestamps null: false
     end
