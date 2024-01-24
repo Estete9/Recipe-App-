f72655b3dc720c0232569fd4b33491bc84bf63f4
Esteban <cepalacios@outlook.com>
2024-01-24 18:50:27 -0500

    Remove unnecessary migrations and fix foreign_key for recipes

--- db/migrate/20240124182642_add_user_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToRecipes < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :recipes, :user, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_054627) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_24_192450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,9 +40,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_054627) do
     t.text "cooking_time"
     t.text "description"
     t.boolean "public"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
