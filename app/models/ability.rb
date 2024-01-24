661f0e622286b85f5bf860adf679c279ae11d94c
Jose RM <mojica.raul@gmail.com>
2024-01-24 14:49:37 -0600

    Fixed issue in the  authorization in the ability file to food

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,9 +8,9 @@ class Ability
     can :read, Inventory
     can :destroy, Inventory, user_id: user.id
 
-    can :create, Foods
-    can :read, Foods
-    can :destroy, Foods, user_id: user.id
+    can :create, Food
+    can :read, Food
+    can :destroy, Food, user_id: user.id
 
     # Define abilities for the user here. For example:
     #
