76312c910652f131eb0c457ce4394e28abbca5f2
Esteban <cepalacios@outlook.com>
2024-01-25 10:25:32 -0500

    Implement food_inventories#create action

--- app/controllers/food_inventories_controller.rb
+++ app/controllers/food_inventories_controller.rb
@@ -6,4 +6,21 @@ class FoodInventoriesController < ApplicationController
     @foods = Food.all
   end
 
+  def create
+    @food_inventory = FoodInventory.new(food_inventory_params)
+    if @food_inventory.save
+      redirect_to inventory_path(id: @food_inventory.inventory_id)
+    else
+      render :new, alert: 'Failed to add ingredient'
+    end
+
+
+  end
+
+  private
+
+  def food_inventory_params
+    params.require(:food_inventory).permit(:quantity, :inventory_id, :food_id)
+  end
+
 end
