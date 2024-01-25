294f10c5de8881eaa0f9b1bdaeb66fd3b402c246
Esteban <cepalacios@outlook.com>
2024-01-25 10:07:40 -0500

    Implement migration, controller, model, view, and routes for food_inventories

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_192450) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_120300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "food_inventories", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_food_inventories_on_food_id"
+    t.index ["inventory_id"], name: "index_food_inventories_on_inventory_id"
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -59,6 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_192450) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "food_inventories", "foods"
+  add_foreign_key "food_inventories", "inventories"
   add_foreign_key "foods", "users"
   add_foreign_key "inventories", "users"
   add_foreign_key "recipes", "users"
