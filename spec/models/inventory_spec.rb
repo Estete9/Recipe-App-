23f864d7749521d6a7bda693f01ab488a656430f
Esteban <cepalacios@outlook.com>
2024-01-26 15:21:13 -0500

    Implement tests for inventory model

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Inventory, type: :model do
     inventory = build(:inventory, name: nil)
     expect(inventory).to_not be_valid
   end
+
+  it 'has many food_inventories' do
+    association = Inventory.reflect_on_association(:food_inventories)
+    expect(association.macro).to eq(:has_many)
+  end
+
+  it 'belongs to a user' do
+    association = Inventory.reflect_on_association(:user)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+  it 'has many foods through food_inventories' do
+    association = Inventory.reflect_on_association(:foods)
+    expect(association.macro).to eq(:has_many)
+    expect(association.options[:through]).to eq(:food_inventories)
+  end
+
+
 end
