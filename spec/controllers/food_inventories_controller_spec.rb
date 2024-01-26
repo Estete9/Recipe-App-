e776104bca1695b689040a740a09d6125aa8cd17
Esteban <cepalacios@outlook.com>
2024-01-26 17:29:15 -0500

    Implement tests for food_inventories controller

--- spec/controllers/food_inventories_controller_spec.rb
+++ spec/controllers/food_inventories_controller_spec.rb
@@ -2,5 +2,63 @@
 require 'rails_helper'
 
 RSpec.describe FoodInventoriesController, type: :controller do
-  # Add your tests here
+  let(:user) { create(:user) }
+  let(:inventory) { create(:inventory, user: user) }
+  let(:food) { create(:food, user: user) }
+  let(:food_inventory) { create(:food_inventory, inventory: inventory, food: food) }
+
+  before do
+    sign_in(user)
+  end
+
+  describe 'GET #new' do
+    it 'returns a success response' do
+      get :new, params: { inventory_id: inventory.id }
+      expect(response).to be_successful
+    end
+
+    # Add more tests as needed
+  end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      it 'creates a new food_inventory' do
+        expect do
+          post :create, params: { food_inventory: attributes_for(:food_inventory, inventory_id: inventory.id, food_id: food.id) }
+        end.to change(FoodInventory, :count).by(1)
+      end
+
+      it 'redirects to the inventory show page' do
+        post :create, params: { food_inventory: attributes_for(:food_inventory, inventory_id: inventory.id, food_id: food.id) }
+        expect(response).to redirect_to(inventory_path(inventory))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new food_inventory' do
+        expect do
+          post :create, params: { food_inventory: { quantity: nil, inventory_id: inventory.id, food_id: food.id } }
+        end.to_not change(FoodInventory, :count)
+      end
+
+      it 'renders the new template' do
+        post :create, params: { food_inventory: { quantity: nil, inventory_id: inventory.id, food_id: food.id } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested food_inventory' do
+      food_inventory_to_destroy = create(:food_inventory, inventory: inventory, food: food)
+      expect do
+        delete :destroy, params: { id: food_inventory_to_destroy.id }
+      end.to change(FoodInventory, :count).by(-1)
+    end
+
+    it 'redirects to the inventory show page' do
+      delete :destroy, params: { id: food_inventory.id }
+      expect(response).to redirect_to(inventory_path(inventory))
+    end
+  end
 end
