ec53832fb26bea11d93d7e67e7e6696a340d825e
Erwin Amador <eamador0305@gmail.com>
2024-01-26 19:55:32 -0600

    fix linter errors of comprexity and length

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,47 +9,34 @@ class RecipesController < ApplicationController
   end
 
   def shopping_list
-    @recipe = Recipe.includes(:recipe_foods).find(params[:recipe_id])
-    @inventory = Inventory.includes(:food_inventories).find(params[:inventory_id])
-
+    load_recipe_and_inventory
     @missing_foods = calculate_quantity_differences(@recipe, @inventory)
     @total_missing_foods = @missing_foods.length
     @total_price = calculate_total_price(@missing_foods)
   end
 
-  # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.where(user_id: current_user.id)
   end
 
-  # GET /recipes/1 or /recipes/1.json
   def show
-    @recipe = Recipe.includes(:recipe_foods).find(params[:id])
+    load_recipe
     @recipe_food = @recipe.recipe_foods.includes(:food)
   end
 
-  # GET /recipes/new
   def new
     @recipe = Recipe.new
   end
 
-  # POST /recipes or /recipes.json
   def create
     @user = current_user
     @recipe = Recipe.new(recipe_params.merge(user: @user))
 
     respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+      handle_recipe_saving(format)
     end
   end
 
-  # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe.destroy!
 
@@ -60,25 +47,40 @@ class RecipesController < ApplicationController
   end
 
   def general_shopping_list
-    @recipes = current_user.recipes
-    @inventories = current_user.inventories
-
+    load_user_recipes_and_inventories
     @missing_foods = general_calculate_quantity_differences(@recipes, @inventories)
     @total_missing_foods = @missing_foods.length
     @total_price = calculate_total_price(@missing_foods)
-
- 
   end
 
   private
 
-  def calculate_total_price(differences)
-    total_price = 0
-    differences.each do |item|
-      total_price += item[:price]
-    end
+  def load_recipe
+    @recipe = Recipe.includes(:recipe_foods).find(params[:id])
+  end
 
-    total_price
+  def load_recipe_and_inventory
+    @recipe = Recipe.includes(:recipe_foods).find(params[:recipe_id])
+    @inventory = Inventory.includes(:food_inventories).find(params[:inventory_id])
+  end
+
+  def load_user_recipes_and_inventories
+    @recipes = current_user.recipes
+    @inventories = current_user.inventories
+  end
+
+  def handle_recipe_saving(format)
+    if @recipe.save
+      format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+      format.json { render :show, status: :created, location: @recipe }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @recipe.errors, status: :unprocessable_entity }
+    end
+  end
+
+  def calculate_total_price(differences)
+    differences.sum { |item| item[:price] }
   end
 
   def calculate_quantity_differences(recipe, inventory)
@@ -86,77 +88,66 @@ class RecipesController < ApplicationController
 
     recipe.recipe_foods.each do |recipe_food|
       food_inventory = inventory.food_inventories.find_by(food: recipe_food.food)
+      quantity_difference = calculate_quantity_difference(recipe_food, food_inventory)
+      next unless quantity_difference.positive?
 
-      quantity_difference =
-        if food_inventory.nil?
-          recipe_food.quantity
-        else
-          recipe_food.quantity - food_inventory.quantity
-        end
-
-        if quantity_difference.positive?
-          differences << {
-            food: recipe_food.food,
-            quantity_difference: quantity_difference,
-            price: recipe_food.food.price * quantity_difference
-          }
-        end
+      differences << {
+        food: recipe_food.food,
+        quantity_difference:,
+        price: recipe_food.food.price * quantity_difference
+      }
     end
 
     differences
   end
 
-def general_calculate_quantity_differences(recipes, inventories)
-  @unique_foods = {}
-  differences = []
-  
+  def calculate_quantity_difference(recipe_food, food_inventory)
+    food_inventory.nil? ? recipe_food.quantity : recipe_food.quantity - food_inventory.quantity
+  end
 
-  recipes.each do |recipe|
-    inventories.each do |inventory|
-      food_inventories = inventory.food_inventories.where(food: recipe.recipe_foods.map(&:food))
+  def general_calculate_quantity_differences(recipes, inventories)
+    unique_foods = {}
 
-      food_inventories.each do |food_inventory|
-        food = food_inventory.food
-        quantity_difference = recipe.recipe_foods.find_by(food: food).quantity - food_inventory.quantity
+    recipes.each do |recipe|
+      inventories.each do |inventory|
+        calculate_general_quantity_differences(unique_foods, recipe, inventory)
+      end
+    end
 
-        if quantity_difference.positive?
-          if @unique_foods.key?(food.id)
-          @unique_foods[food.id][:quantity_difference] += quantity_difference
-        
-          else
-            @unique_foods[food.id] = {
-              food: food,
-              quantity_difference: quantity_difference,  
-            }
-            
-          end
-          @unique_foods[food.id][:price] = food.price 
-        end
-        
+    unique_foods.values.map do |unique_food|
+      {
+        food: unique_food[:food],
+        quantity_difference: unique_food[:quantity_difference],
+        price: unique_food[:quantity_difference] * unique_food[:price]
+      }
+    end
+  end
+
+  def calculate_general_quantity_differences(unique_foods, recipe, inventory)
+    food_inventories = inventory.food_inventories.where(food: recipe.recipe_foods.map(&:food))
+
+    food_inventories.each do |food_inventory|
+      food = food_inventory.food
+      quantity_difference = food_inventory.quantity - recipe.recipe_foods.find_by(food:).quantity
+
+      next unless quantity_difference.positive?
+
+      if unique_foods.key?(food.id)
+        unique_foods[food.id][:quantity_difference] += quantity_difference
+      else
+        unique_foods[food.id] = {
+          food:,
+          quantity_difference:,
+          price: food.price * quantity_difference
+        }
       end
     end
   end
-  @unique_foods.values.each do |unique_food|
-  differences << {
-    food: unique_food[:food],
-    quantity_difference: unique_food[:quantity_difference],
-    price: unique_food[:quantity_difference] * unique_food[:price] 
-  }
-end
-  differences
-  end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_recipe
-    @recipe = Recipe.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user)
   end
