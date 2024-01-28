1af3273554520ff64fb13b768743c40be3959e91
Erwin Amador <eamador0305@gmail.com>
2024-01-27 18:34:24 -0600

    delete repeated migration

--- db/migrate/20240127185145_remove_foreign_key_and_add_cascade_to_recipe_foods.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeyAndAddCascadeToRecipeFoods < ActiveRecord::Migration[7.1]
-  def up
-    remove_foreign_key :recipe_foods, :recipes
-    add_foreign_key :recipe_foods, :recipes, on_delete: :cascade
-  end
-end
